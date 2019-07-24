2ed706206854327f8cd5793a0dfb26cf5584eb2e
vip6111111 <601115211@qq.com>
2019-07-24 16:06:33 +0800

    '初始提交'

---
+++ README.md
@@ -0,0 +1,44 @@
+# yii2-seasLog extension
+
+使用SeasLog高效日志扩展替换Yii2框架的日志模块，使其提高写日志效率。只需配置就可以实现。
+
+ SeasLog扩展<https://github.com/SeasX/SeasLog/>
+
+## Requirements
+
+- Yii2
+- PHP >=5.4
+- ext-seaslog installed
+
+## Installation
+
+## Usage
+
+main.php
+
+```php
+    'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                'default' => [
+                    'logVars' => [],
+                    'class' => 'wxxiong6\yii\seaslog\Yii2SeasLog',
+                    'levels' => ['trace', 'info', 'error', 'warning'],
+                    'logFile' => 'backend/runtime/logs/',
+                    'logVars' => [],
+                ],
+            ],
+        ],
+```
+
+为了跟yii2日志格式一致,修改php.ini seaslog日志格式
+
+```ini
+seaslog.default_template = %T %M
+```
+
+## Changelog
+
+##### Release 1.0 - Changelog
+
+- Official stable release
---
+++ Yii2SeasLog.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * User: wxxiong6@gmail.com
+ * Date: 2019/7/22
+ * Time: 17:16.
+ */
+
+namespace wxxiong6\yii\seaslog;
+
+use Yii;
+use yii\helpers\VarDumper;
+use yii\log\Logger;
+use yii\log\Target;
+
+class Yii2SeasLog extends Target
+{
+    public $logFile;
+    public $maxLogFiles;
+    public $maxFileSize;
+
+    public $except = [];
+
+    public $log;
+    public $logLevel;
+
+    public function init()
+    {
+        parent::init();
+        if (null === $this->logFile) {
+            $this->logFile = Yii::$app->getRuntimePath().'/';
+        } else {
+            $this->logFile = Yii::getAlias($this->logFile);
+        }
+        if ($this->maxLogFiles < 1) {
+            $this->maxLogFiles = 1;
+        }
+        if ($this->maxFileSize < 1) {
+            $this->maxFileSize = 1;
+        }
+
+        $this->log = Yii::createObject('SeasLog');
+        $this->log->setBasePath(dirname($this->logFile));
+    }
+
+
+    public function export()
+    {
+        foreach ($this->messages as $text) {
+            $text = $this->formatMessage($text);
+            $this->log->log($this->logLevel, $text);
+        }
+    }
+
+    public function formatMessage($message)
+    {
+        list($text, $level, $category) = $message;
+
+        $level = Logger::getLevelName($level);
+        $this->logLevel = $level;
+
+        if (!is_string($text)) {
+            if ($text instanceof \Throwable || $text instanceof \Exception) {
+                $text = (string) $text;
+            } else {
+                $text = VarDumper::export($text);
+            }
+        }
+        $traces = [];
+        if (isset($message[4])) {
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+        }
+
+        $prefix = $this->getMessagePrefix($message);
+
+        return "{$prefix}[$level][$category] $text"
+            .(empty($traces) ? '' : "\n    ".implode("\n    ", $traces));
+    }
+}
